6505dec1e6ec3a9e22ccc4b1ca557a89823f0db5
ArkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2023-03-29 17:42:02 +0200

    Add image upload for user avatar and improve styles

--- src/components/User/UserPanel.js
+++ src/components/User/UserPanel.js
@@ -1,69 +1,71 @@
-import Avatar from './Avatar';
-import { useState } from 'react';
-import {
-	Menu,
-	UnstyledButton,
-	rem,
-	MantineProvider,
-	Flex,
-} from '@mantine/core';
-import { FileButton } from '@mantine/core';
-import { IconUpload, IconLogout } from '@tabler/icons-react';
+import Avatar from './Avatar'
+import { Menu, rem, MantineProvider, Flex, FileButton, Button, UnstyledButton, Text } from '@mantine/core'
+import { IconUpload, IconLogout } from '@tabler/icons-react'
+import { useEffect } from 'react'
+import { useCallback } from 'react'
 
-const UserPanel = ({ loggedUserInfo, handleLogout }) => {
-	const [file, setFile] = useState(null);
+const UserPanel = ({ loggedUserInfo, handleLogout, uploadUserAvatar, setFile, file }) => {
+
+	useEffect(() => {
+		if (file && loggedUserInfo.id) {
+			uploadUserAvatar(loggedUserInfo.id)
+		}
+	}, [file, loggedUserInfo.id])
+
 	return (
-		<MantineProvider theme={{ colorScheme: 'dark' }}>
-			<div
-				style={{
-					border: '4px solid #ccc',
-					borderRadius: '50%',
-					cursor: 'pointer',
-				}}
-				// onClick={() => {
-				// 	console.log('hejka');
-				// }}
-			>
-				<Menu>
+		<MantineProvider theme={{ colorScheme: 'dark',  fontFamily: 'Lato' }} >
+			<div>
+				<Menu closeOnItemClick={false}>
 					<Menu.Target>
-						<button
-							style={{ borderRadius: '50%', border: 'none', cursor: 'pointer' }}
-						>
-							<Avatar
-								firstName={loggedUserInfo.firstName}
-								lastName={loggedUserInfo.lastName}
-								avatarColor={loggedUserInfo.avatarColor}
-								enabledTooltip={false}
-							/>
-						</button>
+						<div>
+							<button style={{ display: 'flex', cursor: 'pointer', background: 'none', border: 'none' }}>
+								<Avatar
+									firstName={loggedUserInfo.firstName}
+									lastName={loggedUserInfo.lastName}
+									avatarColor={loggedUserInfo.avatarColor}
+									enabledTooltip={false}
+									avatarUrl={loggedUserInfo.avatarUrl}
+								/>
+								<div
+									style={{
+										display: 'flex',
+										flexDirection: 'column',
+										marginLeft: '5px',
+										alignItems: 'flex-start',
+										justifyContent: 'center',
+										height: '40px',
+										marginRight: '5px',
+										color: '#e4e4e4',
+									}}>
+									<Text fz={16} fw={500}>
+										{loggedUserInfo.firstName} {loggedUserInfo.lastName}
+									</Text>
+									<Text fz={12} fw={200}>{loggedUserInfo.email}</Text>
+								</div>
+							</button>
+						</div>
 					</Menu.Target>
 
 					<Menu.Dropdown>
 						<Flex direction='column'>
 							<Menu.Item icon={<IconUpload size={rem(16)} />}>
 								<FileButton onChange={setFile} accept='image/png,image/jpeg'>
-									{(props) => (
+									{props => (
 										<UnstyledButton style={{ fontSize: '0.875rem' }} {...props}>
-											Aktualizuj avatar
+											Zmień avatar
 										</UnstyledButton>
 									)}
 								</FileButton>
 							</Menu.Item>
-							<Menu.Item
-								color='red'
-								onClick={handleLogout}
-								icon={<IconLogout size={rem(16)} />}
-							>
+							<Menu.Item color='red' onClick={handleLogout} icon={<IconLogout size={rem(16)} />}>
 								Wyloguj się
 							</Menu.Item>
-
-							{/* Other items ... */}
 						</Flex>
 					</Menu.Dropdown>
 				</Menu>
 			</div>
 		</MantineProvider>
-	);
-};
+	)
+}
 
-export default UserPanel;
+export default UserPanel
